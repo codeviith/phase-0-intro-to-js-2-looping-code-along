6814ad0b5ca2507a4335d306434967fc00d58acb
codeviith <codeviith@Cinya>
2023-07-22 02:27:46 -0400

    JS Lab 9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+function writeCards(array, string) {
+    let newArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+
+    return newArr;
+}
+
+
+function countDown(n) {
+    let i = 0;
+
+    while (i <= n) {
+        console.log(i);
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
